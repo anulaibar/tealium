f5b8170bf8ecd0711b74ba78d418cd79423da17f
Olle Hellgren <olle.hellgren@blocket.se>
2016-09-06 22:12:54 +0200

    Added separate endpoints for dev and QA

--- app.rb
+++ app.rb
@@ -17,19 +17,46 @@ end
 redis = Redis.new
 
 get '/' do
-  redis.get('json')
+  {endpoints: [
+    "#{request.base_url}/dev",
+    "#{request.base_url}/qa",
+    "#{request.base_url}/dev/flush",
+    "#{request.base_url}/qa/flush"
+  ]}.to_json
 end
 
-get '/flush' do
-  redis.set('json', '[]')
-  {message: 'Request log flushed.'}.to_json
+get '/dev' do
+  redis.get('dev')
 end
 
-post '/' do
+get '/qa' do
+  redis.get('qa')
+end
+
+get '/dev/flush' do
+  redis.set('dev', '[]')
+  {message: 'Deleted dev requests.'}.to_json
+end
+
+get '/qa/flush' do
+  redis.set('qa', '[]')
+  {message: 'Deleted QA requests.'}.to_json
+end
+
+post '/dev' do
   # handle åäö
   param = request.params['data'].force_encoding("ISO-8859-1").encode("UTF-8")
   data = JSON.parse(param)['data']
-  json = JSON.parse(redis.get('json'))
+  json = JSON.parse(redis.get('dev'))
   json.unshift(data)
-  redis.set('json', json.to_json)
+  redis.set('dev', json.to_json)
+end
+
+post '/qa' do
+  # handle åäö
+  param = request.params['data'].force_encoding("ISO-8859-1").encode("UTF-8")
+  data = JSON.parse(param)['data']
+  json = JSON.parse(redis.get('qa'))
+  json.unshift(data)
+  redis.set('qa', json.to_json)
 end
